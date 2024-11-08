eff9ebc2f7bd21bf54a9df3f351e8898575daf3e
nurlanek <nurlanbek1@gmail.com>
2024-11-08 22:04:51 +0600

    upd logo and favicon

--- account/admin.py
+++ account/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.models import User
 
-# Register your models here.
+# UserAdmin'i genişletin
+class CustomUserAdmin(UserAdmin):
+    list_display = ('username', 'email', 'first_name', 'last_name', 'is_staff', 'list_groups')
+
+    # Kullanıcının gruplarını gösteren bir metot tanımlayın
+    def list_groups(self, obj):
+        return ", ".join([group.name for group in obj.groups.all()])
+    list_groups.short_description = 'Groups'
+
+# Admin panelinde UserAdmin'i özelleştirilmiş versiyonuyla yeniden kaydedin
+admin.site.unregister(User)
+admin.site.register(User, CustomUserAdmin)
--- account/views.py
+++ account/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.models import User
-from django.urls import reverse
 from django.contrib.auth.models import Group
 from django.db.models.signals import post_save
 from django.dispatch import receiver
@@ -20,17 +19,17 @@ def login_request(request):
             return redirect_based_on_group(request)  # Girişten sonra yönlendirme fonksiyonu
         else:
             return render(request, "account/login.html", {
-                "error": "имя или пароль неправильно"
+                "error": "аты же сырсөз туура эмес"
             })
 
     return render(request, "account/login.html")
 
 def redirect_based_on_group(request):
     user = request.user
-    if user.groups.filter(name='Applying').exists():
+    if user.groups.filter(name='Долбоорчулар').exists():
         # Applying grubundaki kullanıcılar için yönlendirme
         return redirect('regproje:menin_dolboorum')
-    elif user.groups.filter(name='Approvers').exists():
+    elif user.groups.filter(name='Комиссия').exists():
         # Approvers grubundaki kullanıcılar için yönlendirme
         return redirect('regproje:index')
     else:
@@ -48,17 +47,17 @@ def register_request(request):
 
         if password == repassword:
             if User.objects.filter(username=username).exists():
-                return render(request, 'account/register.html', {"error": "такой пользователь уже существует"})
+                return render(request, 'account/register.html', {"error": "бул колдонуучу-ат мурунтан эле бар"})
             else:
                 if User.objects.filter(email=email).exists():
-                    return render(request, 'account/register.html', {"error": "такой эл.почта уже существует"})
+                    return render(request, 'account/register.html', {"error": "бул эл.куржун мурунтан эле бар"})
                 else:
                     user = User.objects.create_user(username=username, email=email, first_name=firstname, last_name=lastname, password=password)
                     user.save()
                     return redirect("account:login")
 
         else:
-            return render(request, 'account/register.html', {"error":"Пароль не совпадает"})
+            return render(request, 'account/register.html', {"error":"Сыр сөз туура келбейт"})
 
     return render(request, "account/register.html")
 
@@ -70,5 +69,5 @@ def logout_request(request):
 def add_user_to_applying_group(sender, instance, created, **kwargs):
     if created:
         # "Applying" adlı bir grup oluşturduğunuzdan emin olun
-        group, created = Group.objects.get_or_create(name='Applying')
+        group, created = Group.objects.get_or_create(name='Долбоорчулар')
         instance.groups.add(group)
